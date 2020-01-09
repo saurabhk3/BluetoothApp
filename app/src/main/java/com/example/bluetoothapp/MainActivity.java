68ad8966852b64775129546b2d38fe309533303a
saurabhk3 <kannaujia944@gmail.com>
2020-01-09 12:15:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bluetoothapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.bluetoothapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
