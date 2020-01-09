fb04dcef5f7472c7225477d139c6cdda57d4306e
saurabhk3 <kannaujia944@gmail.com>
2020-01-09 13:19:48 +0530

    Nearby Devices are shown in logcat!

--- app/src/main/java/com/example/bluetoothapp/MainActivity.java
+++ app/src/main/java/com/example/bluetoothapp/MainActivity.java
@@ -1,20 +1,77 @@
 package com.example.bluetoothapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    TextView searchTextView;
+    ListView listView;
+    Button searchButton;
+    BluetoothAdapter bluetoothAdapter;
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+       @Override
+       public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.i("Action::",action);
+            if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)){
+                searchTextView.setText("Finished!");
+                searchButton.setEnabled(true);
+            }
+            else if(BluetoothDevice.ACTION_FOUND.equals(action)){
+
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                Log.i("Device","name:"+device.getName()+" Address:"+device.getAddress());
+            }
+       }
+   };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        searchTextView = findViewById(R.id.search);
+        searchButton = findViewById(R.id.searchButton);
+        listView = findViewById(R.id.listView);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+
+        registerReceiver(receiver,filter);
+
+    }
 
 
+    public void searchDevice(View view){
+        searchTextView.setText("Searching...");
+        searchButton.setEnabled(false);
 
+        int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+        bluetoothAdapter.startDiscovery();
     }
 }
